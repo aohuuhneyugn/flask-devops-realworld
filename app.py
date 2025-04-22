df2574d3addf89cef56c323868599fa7ff8db466
aohuuhneyugn <112865451+aohuuhneyugn@users.noreply.github.com>
2025-04-22 12:06:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+FROM python:3.10-slim
 
-app = Flask(__name__)
+WORKDIR /app
 
-@app.route('/')
-def hello():
-    return "Hello from DevOps Realworld!"
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+COPY app/ ./app
+CMD ["python", "app/app.py"]
